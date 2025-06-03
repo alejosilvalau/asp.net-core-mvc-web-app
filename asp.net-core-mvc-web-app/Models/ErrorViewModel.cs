fcd065fb130b46c72a0edf69b1e44807417221c6
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-03 19:49:20 -0300

    First commit

---
+++ asp.net-core-mvc-web-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_core_mvc_web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
