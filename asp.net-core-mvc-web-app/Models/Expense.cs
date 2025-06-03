5546a54362d8a5b10b67add755e9ae8db9b499c5
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-03 20:13:57 -0300

    Added the Expense model

---
+++ asp.net-core-mvc-web-app/Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace asp.net_core_mvc_web_app.Models
+{
+  public class Expense
+  {
+    public int Id { get; set; }
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    [Required]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Amount needs to be higher than 0")]
+    public double Amount { get; set; }
+    [Required]
+    public string Category { get; set; } = string.Empty;
+    public DateTime Date { get; set; } = DateTime.Now;
+  }
+}
