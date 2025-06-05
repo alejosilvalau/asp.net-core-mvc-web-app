073e8812ca2bbf53b152295eaeb16d5e6178c721
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-05 18:50:01 -0300

    Creating the first POST method

--- FinanceApp/Controllers/ExpensesController.cs
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -14,6 +14,12 @@ namespace FinanceApp.Controllers
 
     public IActionResult Index()
     {
+      var expenses = _context.Expenses.ToList();
+      return View(expenses);
+    }
+
+    public IActionResult Create()
+    {
       return View();
     }
   }
