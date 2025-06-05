7a17a336e29cc6fcac210775e0798f5de84eebf3
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-04 22:13:15 -0300

    Started adding context and testing it locally with Microsoft SQL Server Management

---
+++ asp.net-core-mvc-web-app/Data/FinanceAppContext.cs
@@ -0,0 +1,12 @@
+﻿using asp.net_core_mvc_web_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp.net_core_mvc_web_app.Data
+{
+  public class FinanceAppContext : DbContext
+  {
+    public FinanceAppContext(DbContextOptions<FinanceAppContext> options) : base(options) { }
+
+    DbSet<Expense> Expenses { get; set; }
+  }
+}
