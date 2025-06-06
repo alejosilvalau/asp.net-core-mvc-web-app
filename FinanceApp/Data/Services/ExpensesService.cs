e3e665d3cb5e234bb134778c3b1555e4cb0a59e9
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-06 18:45:06 -0300

    Added the interface and basic implementation of the expenses service

---
+++ FinanceApp/Data/Services/ExpensesService.cs
@@ -0,0 +1,18 @@
+﻿using FinanceApp.Models;
+
+namespace FinanceApp.Data.Services {
+  public class ExpensesService : IExpensesService {
+    private readonly FinanceAppContext _context;
+    public ExpensesService(FinanceAppContext context) {
+      _context = context;
+    }
+
+    public Task Add(Expense expense) {
+      throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Expense>> GetAll() {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ FinanceApp/Data/Services/IExpensesService.cs
@@ -0,0 +1,8 @@
+﻿using FinanceApp.Models;
+
+namespace FinanceApp.Data.Services {
+  public interface IExpensesService {
+    Task<IEnumerable<Expense>> GetAll();
+    Task Add(Expense expense);
+  }
+}
