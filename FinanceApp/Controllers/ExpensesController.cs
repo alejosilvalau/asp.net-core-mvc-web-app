9146d0b2ec21d6480df6ad121282587c55bc4dd0
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-06 18:32:27 -0300

    Made the controller methods asynchronous

--- FinanceApp/Controllers/ExpensesController.cs
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -1,6 +1,7 @@
 ﻿using FinanceApp.Data;
 using FinanceApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceApp.Controllers {
   public class ExpensesController : Controller {
@@ -10,8 +11,8 @@ namespace FinanceApp.Controllers {
       _context = context;
     }
 
-    public IActionResult Index() {
-      var expenses = _context.Expenses.ToList();
+    public async Task<IActionResult> Index() {
+      var expenses = await _context.Expenses.ToListAsync();
       return View(expenses);
     }
 
@@ -20,10 +21,10 @@ namespace FinanceApp.Controllers {
     }
 
     [HttpPost]
-    public IActionResult Create(Expense expense) {
+    public async Task<IActionResult> Create(Expense expense) {
       if (ModelState.IsValid) {
         _context.Expenses.Add(expense);
-        _context.SaveChanges();
+        await _context.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
       }
 
